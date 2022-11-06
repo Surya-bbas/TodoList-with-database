e426d3f56eb5f150d3ce98e963b3781649cab64e
Surya-bbas <suryasaravanan2002@gmail.com>
2022-11-06 21:49:17 +0530

    bug fix

--- app.js
+++ app.js
@@ -129,6 +129,6 @@ app.post(`/delete`, async (req,res)=>{
   res.redirect(`/`)
 })
 
-app.listen(3000, function() {
+app.listen(3000 || process.env.PORT , function() {
   console.log("Server started on port 3000");
 });
